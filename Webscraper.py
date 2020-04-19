edf65b9d5c2f3fa89b869719747a68199f247398
jon <jonnie_schwartz@hotmail.com>
2020-04-19 16:20:24 +1000

    Now has Supreme court functionality Also prints matches to .csv Switched off unnecesary printing of court list to csv

--- Webscraper.py
+++ Webscraper.py
@@ -1,4 +1,4 @@
-# TODO can I install dependencies from a python script? ?PIP
+# TODO can I install dependencies from a python script? ?PIP ?Exe builder (rolls in all dependencies)
 
 #package imports
 import requests
@@ -9,36 +9,57 @@ import csv
 
 ## scooping the data from the website using requests package
 # TIP: to extend table length we add ?num_ranks=9999
-url = 'https://www.courts.act.gov.au/magistrates/lists?num_ranks=9999'
-wholesite = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
-html = wholesite.content
+MC_url = 'https://www.courts.act.gov.au/magistrates/lists?num_ranks=9999'
+MC_wholesite = requests.get(MC_url, headers={'User-Agent': 'Mozilla/5.0'})
+MC_html = MC_wholesite.content
+
+SC_url = 'https://www.courts.act.gov.au/supreme/lists?num_ranks=9999'
+SC_wholesite = requests.get(SC_url, headers={'User-Agent': 'Mozilla/5.0'})
+SC_html = SC_wholesite.content
 
 ## pull the data into beautifulsoup which reads website data better then display it formatted nicely using the prettify function
-soup = BeautifulSoup(html, 'html.parser')
-table = soup.find('table', id="search-results")
+MC_soup = BeautifulSoup(MC_html, 'html.parser')
+MC_table = MC_soup.find('table', id="search-results")
+SC_soup = BeautifulSoup(SC_html, 'html.parser')
+SC_table = SC_soup.find('table', id="search-results")
+
+
+# TODO add supreme court functionality. Either append SC data onto 'table' or onto 'courtlist_data'
+
 
 ## Get the text from the table
 # this iterates through the table rows then cells (tr means table row in html, td means individual cell)
 # think of this loop function like running your finger along reading text in a book. You iterate down line by line, but across each word (cell) in the line.
+
 courtlist_data = []
-for row in table.findAll('tr'):
+#MC
+for row in MC_table.findAll('tr'):
+    list_of_cells = []
+    for cell in row.findAll('td'):
+        list_of_cells.append(cell.text)
+    courtlist_data.append(list_of_cells)
+#SC
+for row in SC_table.findAll('tr'):
     list_of_cells = []
     for cell in row.findAll('td'):
         list_of_cells.append(cell.text)
     courtlist_data.append(list_of_cells)
 
+
+
 ## print to .csv using csv module
 # #redundant
-new_file = open("./CourtList.csv", "w", newline='')
-writer = csv.writer(new_file)
-writer.writerow(["Case Number", "Name", "Hearing Date"])  # manually inserting headings instead of looping through 'thead' in the html
-writer.writerows(courtlist_data)
+#new_file = open("./CourtList.csv", "w", newline='')
+#writer = csv.writer(new_file)
+#writer.writerow(["Case Number", "Name", "Hearing Date"])  # manually inserting headings instead of looping through 'thead' in the html
+#writer.writerows(courtlist_data)
 
 ### CHAPTER II: the name finder
 ## builds a list of matches
 # by iterating through the web text list and putting the item into a list of matches when it contains the client name.
 
-client_list = 'Stewart', 'SUPPRESSED', 'Van' #TODO have it read from csv # ?should I turn this whole thing into a function so you can input client list name into terminal?
+client_list = 'Barry', 'harry'
+#TODO have it read from csv # ?should I turn this whole thing into a function so you can input client list name into terminal?
 
 matches = []
 
@@ -47,6 +68,13 @@ def listcompare(client_name, courtdata):
         for cell in row:
             if client_name.casefold() in cell.casefold():
                 matches.append(row)
+
+
+
+#FIXME a bug where if client_list is only one name (as opposed to a list of names) then it iterates through by letter - matching every letter.
+# some options to fix:
+# 1) create a conditional (if string do listcompare, if list do iterate through listcompare)
+# 2) force a single string to be put into a list. append onto a blank []?
 
 for name in client_list:
     listcompare(name, courtlist_data)
@@ -64,4 +92,4 @@ writer.writerow(["Case Number", "Name", "Hearing Date"])
 writer.writerows(matches)
 
 # TODO find out how to make it run on another pc. USB .exe? (see resources x 2)
-# TODO add supreme court functionality (scoop data from supreme court url then append it on to the dataset)
+
